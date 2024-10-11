3bc587fd84d0c491dfda42cd13467a9e5a1b7cfc
Mashkoor <mashkoorimran3@gmail.com>
2024-10-10 23:56:42 -0700

    triangle stars

---
+++ downrighttriangle/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Triangle
+{
+    public static void Main(string[] args)
+
+    {
+        Console.WriteLine("Please Enter the Number");
+        int rows = Convert.ToInt16(Console.ReadLine());
+        for (int i = rows; i > 0; i--)
+        {
+            for (int j = rows; j >= i; j--)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+
+
+
+        //int n = 5;
+        //for (int i = n; i >0; i--)
+        //{
+        //    for (int j = 5; j >= i; j--)
+        //    {
+        //        Console.Write("* ");
+        //    }
+        //    Console.WriteLine();
+        //}
+
+
+        /*  int row = 5;
+          for (int i = row; i > 0; i--)
+          {
+              for(int j=0; j<i; j++)
+              {
+                  Console.Write("* ");
+              }
+              Console.WriteLine();
+          }*/
+    }
+
+}
+
